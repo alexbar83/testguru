ad3a839359814a3549afe503eedeeb16a2f11876
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-11-03 18:12:29 +0100

    scope_validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :test_passage
   has_many :test, through: :test_passage
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
+
+   validates :email, presence: true
   
   def tests_by_level(level)
     tests.where(level: level)
