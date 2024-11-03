797bc8d11a47cc1c0405c8496c76ff846d5252fa
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-11-03 17:32:21 +0100

    association_models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User'
+  has_many :questions
+  has_many :test_passage
+  has_many :user, through: :test_passage
 
   def self.titles_by_category(title)
     join(:category).where(categories: { title: title })
