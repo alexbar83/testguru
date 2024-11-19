f8760ce8e315935b3ac398e279c888f43d3b1011
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-11-19 22:46:33 +0100

    auth

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,8 +1,2 @@
-  def show_result_message(test_passage)
-    test_passage.successfully_completed? ? 'completed successfully' : 'failed'
-  end
-
-  def set_result_color(test_passage)
-    test_passage.successfully_completed? ? 'success' : 'fail'
-  end
-  
+module TestPassagesHelper
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,15 @@ class User < ApplicationRecord
   has_many :test, through: :test_passage
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
 
-   validates :email, presence: true
+  validates :email, presence: true 
+
+  has_secure_password
   
   def tests_by_level(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
