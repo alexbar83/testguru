b5c274c23bc46e60d31e846ffe8dd9c7b6c9a7f1
alexbar83 <115842630+alexbar83@users.noreply.github.com>
2024-12-02 12:27:40 +0100

    heroku

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :answers_count, on: :create
+  #validate :answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
